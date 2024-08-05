4c4a420bb28867453e77570f0680ea562d2b1f9f
PrinceDubey <princedubey1998@gmail.com>
2024-08-05 23:48:28 +0530

    Java caclulator

---
+++ hellWorld.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+// func main() {
+// 	fmt.Println("Hello, world!")
+// }
+
+// func main() {
+// 	var student1 string = "John" //type is string
+// 	var student2 = "Jane"        //type is inferred
+// 	x := 2                       //type is inferred
+
+// 	fmt.Println(student1)
+// 	fmt.Println(student2)
+// 	fmt.Println(x)
+// }
+
+//   := ---> is use for declaration of variables and assign the value at same time
+//   but this one we can only use inside the function
+
+// func main() {
+// 	var a string
+// 	var b int
+// 	var c bool
+
+// 	fmt.Println(a) // default is " "
+// 	fmt.Println(b) //default is 0
+// 	fmt.Println(c) // default is false
+
+// 	a = "Prince"
+// 	fmt.Println(a)
+
+// }
+
+func main() {
+	var a, b, c, d int = 1, 3, 5, 7
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
